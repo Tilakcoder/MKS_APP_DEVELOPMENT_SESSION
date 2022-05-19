a687bd1d3b8dedf643fc68280cfcf18750aaa38d
Tilak <tilakagrawal5803@gmail.com>
2022-05-19 22:39:07 +0530

    Adding Flutter project

---
+++ my_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("JU MAKERSPACE APP DEV SESSION"),
+        ),
+        body: Center(
+          child: Text("Tilak Agrawal"),
+        ),
+      ),
+    ),
+  );
+}
